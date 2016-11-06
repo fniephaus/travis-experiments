a201a7efbb60a8300e8e7c3e21b12a58e416247f
Fabio Niephaus <code@fniephaus.com>
2016-11-06 15:12:20 +0100

    Update set_rtprio_limit.c

--- set_rtprio_limit.c
+++ set_rtprio_limit.c
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/resource.h>
-#define errExit(msg)
-do { perror(msg); exit(EXIT_FAILURE); \
 
-} while (0)
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); \
+ } while (0)
+
 int
 main(int argc, char *argv[])
 {
